f921e7533d977b3c57c88ac08174b89bc6364213
John Koehler <john.koehler@weissmaler.de>
2018-11-24 13:20:51 +0100

    Refactor date_string check

--- values.py
+++ values.py
@@ -6,9 +6,6 @@ from datetime import datetime
 # from code import interact
 
 def normalize_values(values):
-    current_month = str(datetime.today().month)
-    current_year = str(datetime.today().year)
-
     result = {
         'total_created' : 0.0,
         'total_won' : 0.0,
@@ -18,21 +15,16 @@ def normalize_values(values):
     for vals in values:
         for e in vals['data']:
             # interact(local=dict(globals(), **locals()))
-            if e['add_time'].split(' ')[0].split('-')[1] == current_month:
-                    if e['add_time'].split(' ')[0].split('-')[0] == current_year:
-                        result['total_created'] += e['value']
+            if is_current_year_and_month(e['add_time']):
+                result['total_created'] += e['value']
     
             if e['status'] == 'won':
-                if e['won_time'].split(' ')[0].split('-')[1] == current_month:
-                    if e['won_time'].split(' ')[0].split('-')[0] == current_year:
-                        result['total_won'] += e['value']
+                if is_current_year_and_month(e['won_time']):
+                    result['total_won'] += e['value']
     
             elif e['status'] == 'open' and e['expected_close_date'] != None:
-                if e['expected_close_date'].split(' ')[0].split('-')[1] == current_month:
-                    if e['expected_close_date'].split(' ')[0].split('-')[0] == current_year:
-                        if e['probability'] == None:
-                            e['probability'] = 1.0
-                        result['forecast'] += (e['value'] * e['probability'])
+                if is_current_year_and_month(e['expected_close_date']):
+                    result['forecast'] += (e['value'] * (e['probability'] or 0.1))
     return result
 
 def stats(department):
@@ -41,7 +33,6 @@ def stats(department):
     raw_values = []
 
     while items_in_collection:
-        print('Here')
         pd_response_json = json.loads(
             fetch_deals(department, start_of_range).read().decode('utf-8')
         )
@@ -62,3 +53,16 @@ def calculate_stats(**kwargs):
     values = stats(department)
 
     return values
+
+def is_current_month(date_string):
+    current_month = str(datetime.today().month)
+
+    return date_string.split(' ')[0].split('-')[1] == current_month
+
+def is_current_year(date_string):
+    current_year = str(datetime.today().year)
+
+    return date_string.split(' ')[0].split('-')[0] == current_year
+
+def is_current_year_and_month(date_string):
+    return is_current_year(date_string) and is_current_month(date_string)
