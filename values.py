11699c0451f92321790fb1dd2ae540e010845b6d
John Koehler <john.koehler@weissmaler.de>
2018-11-26 09:02:24 +0100

    Hardcoded placeholders

--- values.py
+++ values.py
@@ -7,19 +7,19 @@ from code import interact
 
 def normalize_values(values):
     result = {
-        'a-previous' : {
+        'aaa' : {
             'total_created' : 0.0,
             'total_won' : 0.0,
             'total_target' : 768000.00,
             'total_expected' : 0.0,
         },
-        'b-current' : {
+        'bbb' : {
             'total_created' : 0.0,
             'total_won' : 0.0,
             'total_target' : 768000.00,
             'total_expected' : 0.0,
         },
-        'c-following' : {
+        'ccc' : {
             'total_created' : 0.0,
             'total_won' : 0.0,
             'total_target' : 768000.00,
@@ -31,36 +31,36 @@ def normalize_values(values):
 # ===================================================================================================
 # PREVIOUS MONTH
             # interact(local=dict(globals(), **locals()))
-            if is_month(e['add_time'], 'a-previous'):
-                result['a-previous']['total_created'] += e['value']
+            if is_month(e['add_time'], 'aaa'):
+                result['aaa']['total_created'] += e['value']
     
             if e['status'] == 'won':
-                if is_month(e['won_time'], 'a-previous'):
-                    result['a-previous']['total_won'] += e['value']
+                if is_month(e['won_time'], 'aaa'):
+                    result['aaa']['total_won'] += e['value']
 # ===================================================================================================
 # CURRENT MONTH
-            if is_month(e['add_time'], 'b-current'):
-                result['b-current']['total_created'] += e['value']
+            if is_month(e['add_time'], 'bbb'):
+                result['bbb']['total_created'] += e['value']
     
             if e['status'] == 'won':
-                if is_month(e['won_time'], 'b-current'):
-                    result['b-current']['total_won'] += e['value']
+                if is_month(e['won_time'], 'bbb'):
+                    result['bbb']['total_won'] += e['value']
     
             if e['status'] == 'open' and e['expected_close_date'] != None:
-                if is_month(e['expected_close_date'], 'b-current'):
-                    result['b-current']['total_expected'] += (e['value'] * (e['probability'] or 0.1))
+                if is_month(e['expected_close_date'], 'bbb'):
+                    result['bbb']['total_expected'] += (e['value'] * (e['probability'] or 0.1))
 # ===================================================================================================
 # FOLLOWING MONTH
-            if is_month(e['add_time'], 'c-following'):
-                result['c-following']['total_created'] += e['value']
+            if is_month(e['add_time'], 'ccc'):
+                result['ccc']['total_created'] += e['value']
     
             if e['status'] == 'won':
-                if is_month(e['won_time'], 'c-following'):
-                    result['c-following']['total_won'] += e['value']
+                if is_month(e['won_time'], 'ccc'):
+                    result['ccc']['total_won'] += e['value']
     
             if e['status'] == 'open' and e['expected_close_date'] != None:
-                if is_month(e['expected_close_date'], 'c-following'):
-                    result['c-following']['total_expected'] += (e['value'] * (e['probability'] or 0.1))
+                if is_month(e['expected_close_date'], 'ccc'):
+                    result['ccc']['total_expected'] += (e['value'] * (e['probability'] or 0.1))
     return result
 
 def stats(department):
@@ -92,9 +92,9 @@ def calculate_stats(**kwargs):
 
 def is_month(date_string, expected_month):
     expected_month_mapping = {
-        'b-current' : 0,
-        'c-following' : 1,
-        'a-previous' : -1,
+        'bbb' : 0,
+        'ccc' : 1,
+        'aaa' : -1,
     }
 
     current_year = datetime.today().year
@@ -102,7 +102,7 @@ def is_month(date_string, expected_month):
     # interact(local=dict(globals(), **locals()))
 
     requested_month = get_month_string(expected_month_mapping[expected_month])
-    if expected_month == 'a-previous' and get_month_string(expected_month_mapping['b-current']) == "1":
+    if expected_month == 'aaa' and get_month_string(expected_month_mapping['bbb']) == "1":
         requested_year = str(current_year + expected_month_mapping[expected_month])
     else:
         requested_year = str(current_year)
